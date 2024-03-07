9629dbb690fc18a9bcc8cda490c8515a6bfa9c40
ss7321 <ss7321@skbl-418-002.ph.ic.ac.uk>
2024-03-07 13:57:14 +0000

    create button.s. Same as keypad but for a single button that sets a pin on port H

--- mainButton.s
+++ mainButton.s
@@ -7,6 +7,7 @@ psect	udata_acs   ; reserve data space in access ram
 prev_cycle: ds	1
 on_cycles:  ds	1
 off_cycle:  ds	1
+bit_pos:    ds	1
 
 
 psect	code, abs
@@ -48,14 +49,19 @@ cycle_on:
 cycle_off:
 	movf	btn, W, A
 	cpfseq	prev_cycle, A
-	call	check_off_length ; code for diff
+	goto	check_off_length ; code for diff
 	incf	off_cycles
 	return
 
 check_off_length:
 	movlw	0xF0	; check if off cycles long enough for a new char input
 	cpfsgt	off_cycles, A
-	goto ; 
+	return	;   return if not a new character
+	;   if long enough:
+;	call	enc_finish
+;	call	encrypt_data
+;	call	UART_send
+	clrf	bit_pos
 end	rst
 
 
