ee3394f723e1c965f8a9d2032566d11cb85c9884
ss7321 <ss7321@skbl-418-002.ph.ic.ac.uk>
2024-03-07 10:33:11 +0000

    create button.s. Same as keypad but for a single button that sets a pin on port H

--- main.s
+++ main.s
@@ -1,7 +1,6 @@
 #include <xc.inc>
-
+; edited
 extrn	DAC_Setup, DAC_Int_Hi
-extrn	ADC_Setup, ADC_Read, ADC_Alternate, AMPLH
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
@@ -14,7 +13,6 @@ int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
 start:	call	DAC_Setup
-	call	ADC_Setup
 	clrf	TRISD, A
 ;	movlw	0x0F
 ;	movwf	PORTD, A
@@ -36,7 +34,6 @@ loop:	movlw	0x0F
 	movlw	0x00
 	movwf	PORTD, A
 	
-	call	ADC_Alternate
 	goto	loop	
 	
 end	rst
