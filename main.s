4deb74880f08cec8daf3cb58008708c9fba7c69c
ss7321 <ss7321@skbl-418-002.ph.ic.ac.uk>
2024-03-08 11:44:49 +0000

    set the counter time to run cycle for 20ms

--- main.s
+++ main.s
@@ -8,7 +8,7 @@ extrn	LCD_Setup, LCD_Send_Byte_D
 psect	udata_acs   ; reserve data space in access ram
 prev_cycle: ds	1
 on_cycles:  ds	1
-off_cycles:  ds	1
+off_cycles: ds	1
 bit_pos:    ds	1
 enc_byte:   ds	1
 rand_byte:  ds	1
@@ -22,10 +22,13 @@ rst:	org	0x0000	; reset vector
 	goto	start
 
 int_hi:	org	0x0008	; high vector, no low vector
-
+	return
 	
 start:	call	bt_setup
 	call	LCD_Setup
+	movlw	0x01
+	movwf	bit_pos, A
+	clrf	enc_byte
 	
 
 
@@ -62,7 +65,7 @@ cycle_off:
 
 check_off_length:
 ;   is pause long enough for new character?
-	movlw	0xF0
+	movlw	24
 	cpfsgt	off_cycles, A
 	return	;   return if not a new character
 ;   if long enough:
@@ -79,7 +82,7 @@ check_off_length:
 	
 check_on_length:
 ;   is prev input dot or dash?
-	movlw	0xF0
+	movlw	12  ;	dash if 12 cycles long (12x20ms)
 	cpfsgt	on_cycles, A
 	call	enc_dot
 	call	enc_dash
@@ -87,16 +90,23 @@ check_on_length:
 	return
 
 enc_dash:
-	bsf	enc_byte, bit_pos, A
-	incf	bit_pos, F, A
+	movf	bit_pos, W, A
+	xorwf	enc_byte, F, A
+	rlncf	bit_pos, F, A
 	return
 enc_dot:
-	incf	bit_pos, F, A
+	rlncf	bit_pos, F, A
 	return
 	
 enc_finish:
-	bsf	enc_byte, bit_pos, A
-	clrf	bit_pos, A
+;   set the identifier bit
+	movf	bit_pos, W, A
+	xorwf	enc_byte, F, A
+	rlncf	bit_pos, F, A
+;   set bit position to start at 0000 0001
+	movlw	0x01
+	movwf	bit_pos, A
+;   mov encoded byte to Wreg
 	movf	enc_byte, W, A
 	return
 end	rst
