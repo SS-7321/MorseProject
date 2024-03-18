6a9169a4d662bcebb30e6ab227c6d78a859efbb3
ss7321 <ss7321@skbl-418-002.ph.ic.ac.uk>
2024-03-18 17:06:22 +0000

    Changed all nomenclatures

--- encryption.s
+++ encryption.s
@@ -1,81 +1,79 @@
 #include <xc.inc>
 
-extrn	key, enc_byte
-extrn	UART_Transmit_Byte
-global	ENCL, ENCH, encrypt_setup, encrypt, RNG_counter
+extrn	key, encoded_byte
+extrn	UARTTransmitByte
+global	encrypted_byte_lower, encrypted_byte_higher, EncryptSetup, Encrypt, RNG_counter
 
 psect	udata_acs
 RNG_counter:	ds  1
-ax:	ds  1
-RAND:	ds  1
+MT_coefficient:	ds  1
+random_byte:	ds  1
 
 PSECT	udata_acs_ovr,space=1,ovrld,class=COMRAM
 
-ENCH:	ds  1
-ENCL:	ds  1
+encrypted_byte_higher:	ds  1
+encrypted_byte_lower:	ds  1
 
 
     
-psect	encrypt_code, class=CODE
+psect	Encrypt_code, class=CODE
     
-encrypt_setup:
+EncryptSetup:
 	movlw   0xA0
 	movwf   RNG_counter
 	movlw   0x04
-	movwf   ax, A
+	movwf   MT_coefficient, A
 	
 
-encrypt_reset:
-	clrf	RAND
-	clrf	ENCH
-	clrf	ENCL
+EncryptReset:
+	clrf	random_byte
+	clrf	encrypted_byte_higher
+	clrf	encrypted_byte_lower
 	return
 	
-mix_rand:	;   mix encoded byte and rnadom number
+MixRandomBytes:	;   mix encoded byte and rnadom number
 		;   random: FA and encoded byte: EB
 		;   final bytes are FE BA
 	movlw	0x0F		;   Filter A from FA as OA
-	andwf	RAND, W, A	;   
-	movwf	ENCL, A		;   moves OA to lower encrypted byte (ENCL)
+	andwf	random_byte, W, A	;   
+	movwf	encrypted_byte_lower, A		;   moves OA to lower Encrypted byte (encrypted_byte_lower)
 	
 	movlw	0xF0		;   filter F from FA as F0
-	andwf	RAND, W, A	;
-	movwf	ENCH, A		;   moves that to ENCH
+	andwf	random_byte, W, A	;
+	movwf	encrypted_byte_higher, A		;   moves that to encrypted_byte_higher
 	
 	movlw	0x10		;   splits EB into 0E B0
-	mulwf	enc_byte, A	;
-	movf	PRODH, W, A	;   adds 0E to ENCH to get FE
-	addwf	ENCH, F, A	;
+	mulwf	encoded_byte, A	;
+	movf	PRODH, W, A	;   adds 0E to encrypted_byte_higher to get FE
+	addwf	encrypted_byte_higher, F, A	;
 	
 	movf	PRODL, W, A	;   gets BO from PRODL register
-	addwf	ENCL, F, A	;   adds B0 to ENCL to get BA
+	addwf	encrypted_byte_lower, F, A	;   adds B0 to encrypted_byte_lower to get BA
 	return
 	
-xor_LH:		;   XOR FE with BA
-	movf	ENCL, W, A
-	xorwf	ENCH, F, A
+XORLowHigh:		;   XOR FE with BA
+	movf	encrypted_byte_lower, W, A
+	xorwf	encrypted_byte_higher, F, A
 	return
 	
-mersenne_twister:
-	movf	ax, W, A
-	mulwf	RAND, A
-	movff	PRODL, RAND
+MersenneTwister:
+	movf	MT_coefficient, W, A
+	mulwf	random_byte, A
+	movff	PRODL, random_byte
 	movf	RNG_counter, W, A
-	addwf	RAND, F, A
+	addwf	random_byte, F, A
 	return
 
-encrypt:
-	;call	encrypt_reset
-	;call	mersenne_twister
-	;call	mix_rand
-	;call	xor_LH
-	;movf	ENCL, W, A
-	;call	UART_Transmit_Byte
-	;movf	ENCH, W, A
-	;call	UART_Transmit_Byte
-	movf	enc_byte, W, A
-	call	UART_Transmit_Byte
-	
+Encrypt:
+	call	EncryptReset
+	call	MersenneTwister
+	call	MixRandomBytes
+	call	XORLowHigh
+	movf	encrypted_byte_lower, W, A
+	call	UARTTransmitByte
+	movf	encrypted_byte_higher, W, A
+	call	UARTTransmitByte
+
 	return
 	
 end
