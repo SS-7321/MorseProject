5110017b07aea058a1fff863c38d5e5ec3b0ce38
ss7321 <ss7321@skbl-418-002.ph.ic.ac.uk>
2024-03-18 17:06:28 +0000

    Changed all nomenclatures

--- UART.s
+++ UART.s
@@ -1,16 +1,14 @@
 #include <xc.inc>
-extrn	bt_to_LCD, buz_stop
-global  UART_Setup, UART_Transmit_Byte, m_byte, byte_higher, byte_lower, UART_Int
+global  UARTSetup, UARTTransmitByte, byte_higher, byte_lower, UARTInterrupt
 
 psect	udata_acs   ; reserve data space in access ram
 UART_counter:	ds  1	    ; reserve 1 byte for variable UART_counter
 byte_higher:	ds  1
 byte_lower:	ds  1
-m_byte:	ds  1
 byte_counter:	ds  1
 
 psect	uart_code,class=CODE
-UART_Setup:
+UARTSetup:
     bsf	    SPEN	;   enable
     bcf	    SYNC	;   synchronous
     bcf	    BRGH	;   slow speed
@@ -18,45 +16,46 @@ UART_Setup:
     bcf	    BRG16	;   8-bit generator only
     movlw   103		;   gives 9600 Baud rate (actually 9615)
     movwf   SPBRG1, A	;   set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	;   TX1 pin is output on RG6 pin
-    bsf	    RC1IE		;   interrupt
-    bsf	    CREN		;   Receiving bit
+    bsf	    TRISC, PORTC_TX1_POSN, A	;   TX1 pin is output on RC6 pin
+    bsf	    RC1IE	;   interrupt
+    bsf	    CREN	;   Receiving bit
     bsf	    GIE
     bsf	    PEIE
+    clrf    byte_counter
     
     return
 
-UART_Transmit_Message:	    ;	Message stored at FSR2, length stored in W
+UARTTransmitMessage:	    ;	Message stored at FSR1, length stored in W
     movwf   UART_counter, A
 
-UART_Loop_message:
+UARTLoopMessage:
     movf    POSTINC2, W, A
-    call    UART_Transmit_Byte
+    call    UARTTransmitByte
     decfsz  UART_counter, A
-    bra	    UART_Loop_message
+    bra	    UARTLoopMessage
     return
 
-UART_Transmit_Byte:	    ;	Transmits byte stored in W
+UARTTransmitByte:	    ;	Transmits byte stored in W
     btfss   TX1IF	    ;	TX1IF is set when TXREG1 is empty
-    bra	    UART_Transmit_Byte
+    bra	    UARTTransmitByte
     movwf   TXREG1, A
     return
 
-UART_Int:
+UARTInterrupt:
     btfss   RC1IF		;   check that flag bit is set
     retfie  f			;   if not then return
     
     tstfsz  byte_counter, A	;   check if this is the first or second byte
-    goto    receive_higher_byte	;   goes to respective routines
-    goto    recieve_lower_byte
+    goto    receiveHigherByte	;   goes to respective routines
+    goto    receiveLowerByte
     
-receive_higher_byte:
+receiveHigherByte:
     movff   RCREG1, byte_higher	;   move value from register to higher byte
     clrf    byte_counter, A
     bcf	    RC1IF		;   clear interrupt flag
     retfie  f			;   fast return from interrupts
-    
-receive_lower_byte:
+  
+receiveLowerByte:
     movff   RCREG1, byte_lower	;   move value from register to lower byte
     incf    byte_counter, F, A
     bcf	    RC1IF		;   clear interrupt flag
