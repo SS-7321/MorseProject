6d4f3fba2f9146e293a5db841fe2e8d68770803b
SS7321 <SS7321@skbl-418-002.ph.ic.ac.uk>
2024-03-20 18:01:03 +0000

    for the connection sequence

--- connect.s
+++ connect.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 extrn	StringSetup, StringToLCD	; from string
-extrn	UARTTransmitByte, byte_higher   ; from UART
-extrn	LCDSecondLine, LCDSendByteData	; from LCD
+extrn	UARTTransmitByte, byte_higher, UARTClearBytes   ; from UART
+extrn	LCDClear, LCDSecondLine, LCDSendByteData, DelayMS	; from LCD
 
 global	ConnectSetup, GetConnection
 psect	udata_acs
@@ -16,12 +16,81 @@ ConnectSetup:
 	return
 
 GetConnection:
+	movlw	0x31		    ; 1
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+check0xFF:
+	movlw	0xFF
+	call	UARTTransmitByte    ; transmit byte to check for connection
+	movlw	100		    ; waits 100ms
+	call	DelayMS
 ;   was a specific message received? (FF FF?)
 	tstfsz	byte_higher, A
 ;   if received:
 	goto	receivedFF
 ;   if not received:
-	goto	GetConnection
+	goto	check0xFF
 
 receivedFF:
-    
+	movlw	0x32		    ; 2
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+	movlw	0x3E		    ; >>
+	call	LCDSendByteData
+	movlw	0xFF		    ; waits 100ms
+	call	DelayMS
+	movlw	0x0F
+	call	UARTTransmitByte    ; send confirmation byte
+	
+check0x0F:
+	movlw	0x0F
+;   received connection confirmation byte? (F0 0F?) 
+	cpfseq	byte_higher, A
+;   if not received:
+	goto	check0x0F
+;   if received	:
+	movlw	0x33		    ; 3
+	call	LCDSendByteData
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	movlw	0xFF		    ; waits 255ms
+	call	DelayMS
+	call	LCDClear
+	call	UARTClearBytes
+	return
